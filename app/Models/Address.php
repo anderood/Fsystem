2b47608f35c3bbcd594bad3c2f7dab0e903897a2
Anderson <anderood@hotmail.com>
2024-01-31 20:18:02 -0300

    adicionado relação entre tabela de endereços e usuarios

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Address extends Model
 {
@@ -19,4 +20,9 @@ class Address extends Model
         'state',
         'user_id'
     ];
+
+    public function User(): HasOne
+    {
+        return $this->hasOne("User", "id");
+    }
 }
