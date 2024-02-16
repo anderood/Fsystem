b84b3ebf3944e6fd0cad59f39977a40cb2527122
Anderson <anderood@hotmail.com>
2024-02-16 16:57:08 -0300

    Funcional: Ajustado o Service

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,66 +2,15 @@
 
 namespace App\Services;
 
-class UserService
+class UserService implements UserServiceInterface
 {
-    private int $id;
 
-    private string $fullName;
-
-    private string $phone;
-
-    private string $dateOfBirth;
-
-    private string $email;
-
-    public function getId(): int
+    /**
+     * @param $ids
+     * @return mixed
+     */
+    public function obterDadosBasicosPorIds(array $ids)
     {
-        return $this->id;
+        return "cheguei no UserService";
     }
-
-    public function setId($id): void
-    {
-        $this->id = $id;
-    }
-
-    public function getFullName(): string
-    {
-        return $this->fullName;
-    }
-
-    public function setFullName($fullName): void
-    {
-        $this->fullName = $fullName;
-    }
-
-    public function getPhone(): string
-    {
-        return $this->phone;
-    }
-
-    public function setPhone($phone): void
-    {
-        $this->phone = $phone;
-    }
-
-    public function getDateOfBirth(): string
-    {
-        return $this->dateOfBirth;
-    }
-
-    public function setDateOfBirth($dateOfBirth): void
-    {
-        $this->dateOfBirth = $dateOfBirth;
-    }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public function setEmail($email): void
-    {
-        $this->email = $email;
-    }
-
 }
