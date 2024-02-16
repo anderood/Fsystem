f71197f55cb1ec59e4c5352db67c35c460b4eae8
Anderson <anderood@hotmail.com>
2024-02-16 16:53:52 -0300

    Funcional: ajustado a rota

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-Route::get("/user", [UserController::class, "obterDadosPorIds"]);
+Route::post("/user", [UserController::class, "obterDadosBasicosPorIds"]);
