f9d1bded3f9ed341a4518c28cf37448c292e3037
Anderson <anderood@hotmail.com>
2024-04-03 18:09:13 -0300

    realizado ajuste do cadastro de admins

--- resources/views/admin/list_user_admin.blade.php
+++ resources/views/admin/list_user_admin.blade.php
@@ -5,16 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cadastrados</title>
+    <title>List User</title>
 </head>
 <body>
-    <h1>Usuarios Cadastrados</h1>
-
+    <h1>Perfil do Usuario</h1>
     <ul>
-
-        @foreach($cadastros as $cadastro)
-            <li>{{ $cadastro->name }} - {{ $cadastro->phone }} - {{ $cadastro->email }} - {{ $cadastro->observations }}</li>
-        @endforeach
+        <li></li>
     </ul>
 </body>
 </html>
