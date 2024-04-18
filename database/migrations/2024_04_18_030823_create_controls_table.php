a4e20fe894b795fcecca391cae69e73cabe10f2a
Anderson <anderood@hotmail.com>
2024-04-18 00:12:41 -0300

    criado tabela Controls

---
+++ database/migrations/2024_04_18_030823_create_controls_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateControlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('controls', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->date('date');
+            $table->string('amount');
+            $table->string('observation')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('controls');
+    }
+}
