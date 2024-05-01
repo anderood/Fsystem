0c0e5e827e10e3e2f03553cbfb35d6116d14f070
Anderson <anderood@hotmail.com>
2024-05-01 12:22:04 -0300

    ajuste no layout home

---
+++ resources/views/components/lateral-esquerdo.blade.php
@@ -0,0 +1,27 @@
+<section class="container-esquerdo">
+<div class="row">
+    <div class="container-esquerdo-logo">
+        <h1>Financial System</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="container-esquerdo-menu">
+        <ul>
+            <li class="menu-item"><i class="fas fa-tachometer-alt"></i> <a href="#">Dashboard</a></li>
+            <li class="menu-item"><i class="fas fa-exchange-alt"></i> <a href="#">Transações</a></li>
+            <li class="menu-item"><i class="fas fa-file-alt"></i> <a href="#">Relatórios</a></li>
+            <li class="menu-item"><i class="fas fa-cog"></i> <a href="#">Configurações</a></li>
+            <li class="menu-item"><i class="fas fa-users"></i> <a href="#">Membros</a></li>
+            <li class="menu-item"><i class="fas fa-user"></i> <a href="#">Usuários</a></li>
+        </ul>
+    </div>
+
+</div>
+<div class="row">
+    <div class="container-esquerdo-rodape">
+        <ul>
+            <a href="#"><i class="fas fa-sign-out-alt"></i> Logout</a>
+        </ul>
+    </div>
+</div>
+</section>
