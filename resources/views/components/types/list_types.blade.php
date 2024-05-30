8128e45aaca24dacf17e35d9e98d470c6d01deaa
Anderson <anderood@hotmail.com>
2024-05-30 02:24:26 -0300

    exibindo os tipos cadastrados

--- resources/views/components/types/list_types.blade.php
+++ resources/views/components/types/list_types.blade.php
@@ -1,6 +1,24 @@
-<h1>Tipos de Movimento</h1>
-<ul>
-    @foreach($types as $type)
-        <li>{{ $type->name }}</li>
-    @endforeach
-</ul>
+<div>
+    <h3>Tipos</h3>
+</div>
+<div>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Tipo</th>
+            <th>Descrição</th>
+            <th>Editar</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @foreach($types as $type)
+            <tr>
+                <td> {{ $type->name }}</td>
+                <td> {{ $type->description }}</td>
+                <td><a href="/fsystem/types/{{$type->id}}">Editar</a></td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
