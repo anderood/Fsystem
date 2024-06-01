1fe512f7024357673ec2f93a5238146effb93230
Anderson <anderood@hotmail.com>
2024-06-01 02:42:03 -0300

    listagem de Origens

--- resources/views/components/origins/list_origins.blade.php
+++ resources/views/components/origins/list_origins.blade.php
@@ -1,6 +1,24 @@
-<h1>Origens</h1>
-<ul>
-    @foreach($origins as $origin)
-        <li>{{ $origin->name }} - {{ $origin->description }}</li>
-    @endforeach
-</ul>
+<div>
+    <h3>Origens</h3>
+</div>
+<div>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Titulo</th>
+            <th>Descrição</th>
+            <th>Editar</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @foreach($origins as $origin)
+            <tr>
+                <td> {{ $origin->name }}</td>
+                <td> {{ $origin->description }}</td>
+                <td><a href="/fsystem/origins/{{$origin->id}}">Editar</a></td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
