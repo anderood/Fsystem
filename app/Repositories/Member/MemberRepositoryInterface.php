2ce335d8c581d0bbbe2b457db4f238d0259be9fd
Anderson <andersonprrsantos@gmail.com>
2024-06-14 18:30:56 -0300

    realizado espaçamento

--- app/Repositories/Member/MemberRepositoryInterface.php
+++ app/Repositories/Member/MemberRepositoryInterface.php
@@ -7,8 +7,12 @@ use Illuminate\Http\Request;
 interface MemberRepositoryInterface
 {
     public function allMembers();
+
     public function getMemberById(int $id);
+
     public function createMember(Request $request);
+
     public function updateMember(Request $request, int $id);
+
     public function deleteMember(int $id);
 }
