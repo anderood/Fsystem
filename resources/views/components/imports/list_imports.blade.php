fb72bbb0fccb22b1f1074c9ae00353e295bf87c9
Anderson <andersonprrsantos@gmail.com>
2024-06-29 02:11:42 -0300

    realizado ajuste no form para enviar mais de um dado

--- resources/views/components/imports/list_imports.blade.php
+++ resources/views/components/imports/list_imports.blade.php
@@ -6,7 +6,7 @@
 
 
 
-            @foreach($csvImports as $import)
+
         <table class="table table-striped">
             <thead>
             <tr>
@@ -20,53 +20,48 @@
             </tr>
             </thead>
             <tbody>
-
                 <form action="{{ Route('import.store') }}" method="post" enctype="multipart/form-data">
                     @csrf
-                <tr>
-                    <td> <input type="text" name="title" id="" value="{{ $import['title'] }}"></td>
-                    <td> <input type="text" name="amount" id="" value="{{ $import['amount'] }}"></td>
-                    <td> <input type="text" name="date" id="" value="{{ $import['date'] }}"></td>
-                    <td>
-                        <select name="type_id" id="">
-                            <option value="{{$import['type_id']}}">{{$import['type_id']}}</option>
-                            @foreach($allTypes as $type)
-                                <option value="{{ $type->id }}">{{ $type->name }}</option>
-                            @endforeach
-                        </select>
-                    </td>
-                    <td>
-                        <select name="origin_id" id="">
-                            <option value="{{$import['origin_id']}}">{{$import['origin_id']}}</option>
-                            @foreach($allOrigins as $origin)
-                                <option value="{{ $origin->id }}">{{ $origin->name }}</option>
-                            @endforeach
-                        </select>
-                    </td>
-                    <td>
-                        <select name="movement_id" id="">
-                            <option value="{{$import['movement_id']}}">{{$import['movement_id']}}</option>
-                            @foreach($allMovements as $movements)
-                                <option value="{{ $movements->id }}">{{ $movements->name }}</option>
-                            @endforeach
-                        </select>
-                    </td>
-                    <td>
-                        <select name="member_id" >
-                            @foreach($allMembers as $member)
-                                <option value="{{ $member->id }}">{{ $member->first_name }}</option>
-                            @endforeach
-                        </select>
-                    </td>
-                    <td>
-                        <button type="submit">Salvar</button>
-                    </td>
-                </tr>
+                    @foreach($csvImports as $import)
+                        <tr>
+                            <td> <input type="text" name="title[]" id="" value="{{ $import['title'] }}"></td>
+                            <td> <input type="text" name="amount[]" id="" value="{{ $import['amount'] }}"></td>
+                            <td> <input type="text" name="date[]" id="" value="{{ $import['date'] }}"></td>
+                            <td>
+                                <select name="type_id[]">
+                                    <option selected value={{$import['type_id']}}>{{$import['type_id']}}</option>
+                                    @foreach($allTypes as $type)
+                                        <option value="{{ $type->id }}">{{ $type->name }}</option>
+                                    @endforeach
+                                </select>
+                            </td>
+                            <td>
+                                <select name="origin_id[]">
+                                    <option value="{{$import['origin_id']}}">{{$import['origin_id']}}</option>
+                                    @foreach($allOrigins as $origin)
+                                        <option value="{{ $origin->id }}">{{ $origin->name }}</option>
+                                    @endforeach
+                                </select>
+                            </td>
+                            <td>
+                                <select name="movement_id[]">
+                                    <option value="{{$import['movement_id']}}">{{$import['movement_id']}}</option>
+                                    @foreach($allMovements as $movements)
+                                        <option value="{{ $movements->id }}">{{ $movements->name }}</option>
+                                    @endforeach
+                                </select>
+                            </td>
+                            <td>
+                                <select name="member_id[]">
+                                    @foreach($allMembers as $member)
+                                        <option value="{{ $member->id }}">{{ $member->first_name }}</option>
+                                    @endforeach
+                                </select>
+                            </td>
+                        </tr>
+                    @endforeach
+                    <button type="submit">Salvar</button>
                 </form>
             </tbody>
         </table>
-            @endforeach
-
-
-
 </div>
