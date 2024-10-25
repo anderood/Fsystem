5a16f401001f7fc6e635d3024d8075404fa74e43
Anderson <andersonprrsantos@gmail.com>
2024-10-25 02:38:29 -0300

    ajustando rota para home

---
+++ resources/views/dashboard/home_dash.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.layout-master')
+
+@section('container-esquerdo')
+    @include('components.lateral-esquerdo')
+@endsection
+
+@section('container-central')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard\DashController;
 use App\Http\Controllers\Members\MemberController;
 use App\Http\Controllers\Transactions\TransactionController;
 use App\Http\Controllers\Types\TypeController;
@@ -21,6 +22,8 @@ use Illuminate\Support\Facades\Route;
 Route::get("/ping", function (){
     return 'pong';
 });
+
+Route::get("/", [DashController::class, 'index'])->name('dashboard.home_dash');
 
 Route::get("/members", [MemberController::class, 'index'])->name('members.home_members');
 Route::get("/members/create", [MemberController::class, 'create'])->name('members.create_members');
