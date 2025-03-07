ca399d578f6ab67a27a4356d6d588d0602f8636b
Anderson Santos <anderson.santos@esm.com.br>
2025-03-07 19:29:21 -0300

    Ajustando a interface

--- resources/views/dashboard/home_dash.blade.php
+++ resources/views/dashboard/home_dash.blade.php
@@ -1,8 +1,250 @@
-@extends('layouts.layout-master')
+<!DOCTYPE html>
 
-@section('container-esquerdo')
-    @include('components.lateral-esquerdo')
-@endsection
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <!-- Icons-->
+    <link href="template-v2.0.1/vendors/@coreui/icons/css/coreui-icons.min.css" rel="stylesheet">
+    <link href="template-v2.0.1/vendors/flag-icon-css/css/flag-icon.min.css" rel="stylesheet">
 
-@section('container-central')
-@endsection
+    <link href="js/fontawesome-pro-5.11.2/css/all.css" rel="stylesheet">
+
+    <link href="template-v2.0.1/vendors/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+    <!-- Main styles for this application-->
+    <link href="template-v2.0.1/css/style.min.css#" rel="stylesheet">
+    <link href="template-v2.0.1/vendors/pace-progress/css/pace.min.css" rel="stylesheet">
+    <!-- Bootstrap-table -->
+    <link href="js/bootstrap-table/dist/bootstrap-table.css" rel="stylesheet">
+    <!-- Fancybox -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css"/>
+    <!-- Dateranger picker -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css"/>
+    <!-- Select2 -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet"/>
+    <!-- iCheck -->
+    <link href="js/icheck-1.0.2/skins/minimal/blue.css#" rel="stylesheet">
+
+    <link href="template-v2.0.1/vendors/toastr/css/toastr.min.css#" rel="stylesheet">
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- JQuery UI -->
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+
+    <!-- Custom Style -->
+    <link href="css/custom.css#" rel="stylesheet">
+
+    <!-- Bootstrap and necessary plugins-->
+    <script src="template-v2.0.1/vendors/popper.js/js/popper.min.js#"></script>
+    <script src="template-v2.0.1/vendors/bootstrap/js/bootstrap.min.js#"></script>
+    <script src="template-v2.0.1/vendors/pace-progress/js/pace.min.js#"></script>
+    <script src="template-v2.0.1/vendors/perfect-scrollbar/js/perfect-scrollbar.min.js#"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+
+    <script src="https://twitter.github.io/typeahead.js/releases/latest/typeahead.bundle.js"></script>
+
+    <script src="template-v2.0.1/vendors/select2/js/i18n/pt-BR.js#"></script>
+    <script src="template-v2.0.1/vendors/toastr/js/toastr.js#"></script>
+    <script src="js/icheck-1.0.2/icheck.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <script src="template-v2.0.1/vendors/@coreui/coreui-pro/js/coreui.min.js"></script>
+    <script src="js/bootstrap-table/dist/bootstrap-table.min.js"></script>
+    <script src="js/bootstrap-table/dist/locale/bootstrap-table-pt-BR.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
+    <script src="js/jquery-mask-plugin/dist/jquery.mask.min.js"></script>
+    <script src="js/custom.js?rand='. rand(111111,999999))?>"></script>
+
+</head>
+
+<body class="app _header-fixed _sidebar-fixed _aside-menu-fixed sidebar-lg-show">
+
+<header class="app-header navbar d-print-none">
+    <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">
+        <span style="font-weight: 300; color: #000">F</span>
+        <span style="font-weight: 900; color: #20a8d8">SYSTEM</span>
+    </a>
+    <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <ul class="nav navbar-nav d-md-down-none">
+        <li class="nav-item px-3 d-none">
+            <a class="nav-link" href="#" target="_blank">Template</a>
+        </li>
+    </ul>
+    <ul class="nav navbar-nav ml-auto avatar-nav">
+        <li class="nav-item font-italic d-none d-lg-block">meunome@meuemail.com.br</li>
+        <li class="nav-item dropdown">
+            <a class="nav-link nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" title="">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                </svg>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                <div class="dropdown-header text-center">
+                    <strong>Minha conta</strong>
+                </div>
+                <a class="dropdown-item" href="#">
+                    <i class="far fa-user"></i>
+                    Meus dados
+                </a>
+                <div class="divider"></div>
+                <a class="dropdown-item" href="#">
+                    <i class="far fa-lock"></i>
+                    Sair
+                </a>
+            </div>
+        </li>
+    </ul>
+</header>
+<div class="app-body">
+    <div class="sidebar">
+        <nav class="sidebar-nav">
+            <ul class="nav">
+                <li class="nav-item"><a class="nav-link" href="dashboard'"> Dashboard</a></li>
+                <li class="nav-item nav-dropdown">
+                    <a href="#" class="nav-link nav-dropdown-toggle"> Usuários</a>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item"><a href="#" class="nav-link">Administradores</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Usuários</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item nav-dropdown">
+                    <a href="#" class="nav-link nav-dropdown-toggle"> Conteúdo</a>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item"><a href="#" class="nav-link">Destaques</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Ordem dos Destaques</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Textos</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item nav-dropdown">
+                    <a href="#" class="nav-link nav-dropdown-toggle"> Calendário</a>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item"><a href="#" class="nav-link">Times</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Campeonatos</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Jogos</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Arenas</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item nav-dropdown">
+                    <a href="#" class="nav-link nav-dropdown-toggle"> Elenco</a>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item"><a href="#" class="nav-link">Futebol</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Vôlei</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item nav-item">
+                    <a href="#" class="nav-link"> E-mails</a>
+                </li>
+
+                <li class="nav-item nav-dropdown">
+                    <a href="#" class="nav-link nav-dropdown-toggle"> Marketplace</a>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Vendedores</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Usuários</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Produtos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Itens</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Estoque</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Preços</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Compre Junto</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="d#" class="nav-link">Cupom de Desconto</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Vendas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Relatório</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item nav-dropdown">
+                    <a href="#" class="nav-link nav-dropdown-toggle"> Mídias</a>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item"><a class="nav-link" href="#">Fotos</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Alterar Fotos</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Álbuns de Fotos</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Vídeos</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Wallpapers</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link"><i class="icon-person"></i>Sócio-Torcedores</a>
+                </li>
+                <li class="nav-item"><a href="#" class="nav-link"> Tabelas</a></li>
+                <li class="nav-item"><a href="#" class="nav-link"> Uploads</a></li>
+                <li class="nav-item"><a href="#" class="nav-link" target="_blank"> Template</a></li>
+            </ul>
+        </nav>
+        <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+    </div>
+
+    <main class="main">
+        <ol class="breadcrumb d-print-none">
+            <li class="breadcrumb-item">
+                <a href="#">Home</a>
+            </li>
+        </ol>
+
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-header">
+                    Home
+                </div>
+                <div class="card-body">
+                    Seja bem-vindo(a) do Financial System!
+                </div>
+            </div>
+        </div>
+    </main>
+
+</div>
+<footer class="app-footer footer-fixed justify-content-end">
+    <span class="small">
+        <a href="#" target="_blank"></a> © <?= date('Y') ?> Financial System.</span>
+</footer>
+
+<!-- Modal -->
+<div class="modal fade" id="SysVarModal" tabindex="-1" role="dialog" aria-labelledby="SysVarModal" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="SysVarModal">System Variables</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
